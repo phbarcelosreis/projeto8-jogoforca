9b035475bb6ac19dca959b57882ce1a12af0605f
Pedro Reis <phbarcelosreis66@gmail.com>
2022-09-24 19:19:11 -0300

    Layout refinements

--- src/components/teste.js
+++ src/components/teste.js
@@ -1,18 +1,35 @@
-import { Title } from "./style";
-import { Teste } from "./style";
+import { Letter, Title, Grid, Teste, Page, Imagem } from "./style";
 
-export default function App(){
+
+
+export default function App() {
+    const alfabeto = [
+        "A", "B", "C", "D", "E", "F", "G", "H", "I", "J", "K", "L", "M", "N", "O", "P", "Q", "R", "S", "T", "U", "V", "W", "X", "Y", "Z"
+    ];
+
+
+
+
+
     return (
-        <Teste>
-            <Imagem/>
-            <Title>oi</Title>
-        </Teste>
+        <Page>
+            <Teste>
+                <Imagem src="/assets/forca0.png" alt="Forca-Inicial" />
+                <Title placeholder="Escolher Palavra"/>
+            </Teste>
+            <Grid>
+                {alfabeto.map((props) =>
+                    <Letter letra={props}>{props}</Letter>
+                )}
+            </Grid>
+        </Page>
+
     )
 }
 
-function Imagem(){
-    return <img src="/assets/forca0.png" alt="forca"/>
-}
+/* function Imagem() {
+    return <img src="/assets/forca0.png" alt="forca" />
+} */
 
 
 
